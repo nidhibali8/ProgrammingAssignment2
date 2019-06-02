cb694422779726d0486a67e0932b6447dbc206d7
nidhibali8 <50478031+nidhibali8@users.noreply.github.com>
2019-06-02 16:40:14 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##This funtion is part of Coursera R programming Assignment of Week 3
+##and is used to cache the inverse of a matrix to save long computational time
 
+## This function creates a special matrix object that can cache its inverse
+## This script defines two functions makeCachematrix and cachsolve to return inverse of matrix 
+##from cache if matrix has not changed 
+makecachematrix<-function(x=matrix()){
+  invMat<-NULL 
+  setMat<-function(x=matrix()){
+    x<<-y
+    invMatrix<<-NULL
+  }
+  getMat<-function() x
+  setInverse<-function(inverse) invMat<<-inverse
+  getInverse<-function() invMat 
+  list(setMat=setMat, getMat=getMat, setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This next function retrieves the inverse from the memory cache
+cacheSolve<-function(x,...){
+  invMat<-x$getInverse() ##return inverse matrix of argument matrix x
+  if(!is.null(invMat)){
+   message("getting cached matrix")
+    return(invMat)
+  }
+  Matdata<-x$getMat()
+  invMat<-solve(Matdata,...)
+  print("calling set inverse")
+  x$setInverse(invMat)
+  return(invMat)
 }
